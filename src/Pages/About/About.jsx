93a740181e06cc4de7e3b2e3f5041fc1289de2b1
madihaisawesome <m24049502@gmail.com>
2025-12-08 05:04:58 +0000

    nana

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,13 +1,11 @@
 import Layout from "../../Components/Layout/Layout";
 import Brief from "./Brief/Brief";
-import Gallery from "./Gallery/Gallery";
 import Services from "./Services/Services";
 
 const About = () => {
   return (
     <Layout pageTitle="About Me">
       <Brief />
-      <Gallery />
       <Services />
     </Layout>
   );
