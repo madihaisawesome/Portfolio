00b186fa5c4eda12781f3876cc4e9f8294160867
madihaisawesome <m24049502@gmail.com>
2025-12-08 01:42:01 +0000

    Added Map on Contact Page

--- src/Pages/Contact/MapBox/MapBox.jsx
+++ src/Pages/Contact/MapBox/MapBox.jsx
@@ -5,11 +5,11 @@ const MapBox = () => {
     <section className="mapbox">
       <figure>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d924237.707752053!2d66.49605745642145!3d25.192983914443513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33e06651d4bbf%3A0x9cf92f44555a0c23!2sKarachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e0!3m2!1sen!2s!4v1724896367079!5m2!1sen!2s"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d321203.7052623736!2d-114.41746851454111!3d51.02715964129616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537170039f843fd5%3A0x266d3bb1b652b63a!2sCalgary%2C%20AB!5e0!3m2!1sen!2sca!4v1765158037751!5m2!1sen!2sca" 
           width="400"
           height="300"
           loading="lazy"
-          title="Google Map of Karachi"
+          title="Google Map of Calgary"
         ></iframe>
       </figure>
     </section>
